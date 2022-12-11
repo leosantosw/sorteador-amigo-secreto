eda606c37bc38bc8d3797a5b334a25f71d4609ea
leosantosw <leonardosantosx0077@gmail.com>
2022-12-11 20:33:43 -0300

    feat: implement recoil in tests

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,12 +1,39 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
+import { RecoilRoot } from "recoil"
 import { Form } from "./Form"
 
-test('when input is empty, submit button is disabled', () => {
-  render(<Form />)
-  const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+describe('Form participant', () => {
+  test('when input is empty, submit button is disabled', () => {
+    render(
+      <RecoilRoot>
+        <Form />
+      </RecoilRoot>
+    )
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
 
-  const button = screen.getByRole('button')
+    const button = screen.getByRole('button')
 
-  expect(input).toBeInTheDocument()
-  expect(button).toBeDisabled()
+    expect(input).toBeInTheDocument()
+    expect(button).toBeDisabled()
+  })
+
+  test('add participant to list if input is not empty', () => {
+    render(
+      <RecoilRoot>
+        <Form />
+      </RecoilRoot>
+    )
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    const button = screen.getByRole('button')
+
+    fireEvent.change(input, { 
+      target: { 
+        value: 'John Doe'
+      } 
+    })
+
+    fireEvent.click(button)
+    expect(input).toHaveFocus()
+    expect(input).toHaveValue('')
+  })
 })
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,30 @@
+import { FormEvent, useRef, useState } from "react"
+import { useAddParticipant } from '../../state/hook/useAddParticipant'
+
 export const Form = () => {
+  
+  const [nameParticipant, setNameParticipant] = useState<string>('')
+  const addPartipant = useAddParticipant()
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleAddParticipant = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    addPartipant(nameParticipant)
+
+    setNameParticipant('')
+    inputRef.current?.focus()
+  }
+
   return (
-    <form>
-      <input type="text" placeholder="Insira os nomes dos participantes"/>
-      <button disabled>Adicionar</button>
+    <form onSubmit={handleAddParticipant}>
+      <input
+        ref={inputRef} 
+        type="text" 
+        placeholder="Insira os nomes dos participantes" 
+        onChange={(event) => setNameParticipant(event.target.value)}
+        value={nameParticipant}
+      />
+      <button disabled={!nameParticipant}>Adicionar</button>
     </form>
   )
 }
